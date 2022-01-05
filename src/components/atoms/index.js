b518e59191d887f467ccd0fc86c305499018a755
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-01-04 19:05:02 -0600

    Validacion en InputForm
    Se agregaron funciones de validacion a input form para validaciones en tiempo real

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,4 +1,4 @@
-export {InputForm} from './InputForm';
+export {InputForm, InputValidation} from './InputForm';
 export {ModalContentWrapper} from './ModalContentWrapper';
 export {TermsCheck} from './TermsCheck';
 export {ToSectionMessage} from './ToSectionMessage';
--- src/components/molecules/LogInFields.js
+++ src/components/molecules/LogInFields.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {Button} from 'react-native-elements';
 import {styles, CustomButtonStyle} from '../../styles';
-import {InputForm, TermsCheck} from '../atoms';
+import {InputForm, InputValidation, TermsCheck} from '../atoms';
 
 export const LogInFields = () => {
   return (
     <View>
       <Text style={styles.formHeader}>Log In</Text>
       <View>
-        <InputForm label="Email" />
-        <InputForm label="Password" />
+        <InputForm label="Email" validation={InputValidation.email} />
+        <InputForm label="Password" validation={InputValidation.password} />
       </View>
       <View style={styles.checkboxContainer}>
         <TermsCheck title="Remember me" />
