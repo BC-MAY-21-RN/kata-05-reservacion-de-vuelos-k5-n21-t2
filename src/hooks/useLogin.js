cabcb30e22380f37917c15f8380530afcf2ec8a6
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-01-05 21:48:24 -0600

    Enclosed conditional in function
    To reduce cognitive complexity, a conditional were enclose in a function to pass codeclimate checkout

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,14 +1,17 @@
 import {useState} from 'react';
 
+const allIsOk = (email, password) => {
+  return email.isOk && password.isOk;
+};
+
 const useLogin = () => {
   const [email, setEmail] = useState({value: '', isOk: false});
   const [password, setPassword] = useState({value: '', isOk: false});
   const [submit, setSubmit] = useState(true);
   const setState = (action, value, isOk) => {
     if (action === 'email') setEmail({value, isOk});
-    if (action === 'password') setPassword({value, isOk});
-    const allIsOk = email.isOk && password.isOk;
-    setSubmit(allIsOk ? false : true);
+    else if (action === 'password') setPassword({value, isOk});
+    setSubmit(!allIsOk(email, password));
   };
 
   return [{email, password, submit}, setState];
