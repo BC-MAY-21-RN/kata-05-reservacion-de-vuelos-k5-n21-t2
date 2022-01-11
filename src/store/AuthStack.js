d9f303c878fca4ff415cd0c4983ac06133c24941
Nando San Miguel <contact@namoku.xyz>
2022-01-11 16:55:46 -0600

    Fix google sign in bug

--- src/store/AuthStack.js
+++ src/store/AuthStack.js
@@ -1,18 +1,19 @@
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
-import {auth} from '@react-native-firebase/auth';
+export const AuthStack = (auth, GoogleSignin) => {
+  GoogleSignin.configure({
+    webClientId:
+      '1005940255457-sc10mqe7t0n6h4phh13cvnf076pbjrpn.apps.googleusercontent.com',
+  });
 
-// GoogleSignin.configure({
-//   webClientId:
-//     '1005940255457-sc10mqe7t0n6h4phh13cvnf076pbjrpn.apps.googleusercontent.com',
-// });
+  async function onGoogleButtonPress() {
+    const {idToken} = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    return auth().signInWithCredential(googleCredential);
+  }
 
-export async function onGoogleButtonPress() {
-  // Get the users ID token
-  const {idToken} = await GoogleSignin.signIn();
+  return onGoogleButtonPress;
+};
 
-  // Create a Google credential with the token
-  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(googleCredential);
-}
+export const AuthLogOut = (auth, GoogleSignin) => {
+  auth().signOut();
+  GoogleSignin.signOut();
+};
