36b7287aa9afeed9860ad479b6a6d1745648a8cc
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-01-21 14:51:27 -0600

    Refactor statement's group to reduce cognitive load
    - for codeclimate checkout

--- src/components/atoms/SignLoading.js
+++ src/components/atoms/SignLoading.js
@@ -4,6 +4,16 @@ import {ActivityIndicator, Text} from 'react-native';
 import {styles, CustomOverlayStyle} from '../../styles';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+const getOverlayText = (type, done) => {
+  return type === 'signup'
+    ? done
+      ? 'Signed up'
+      : 'Signin up...'
+    : done
+    ? 'Signed in'
+    : 'Signing in';
+};
+
 const SignLoading = ({state, type}) => {
   const [loading, setLoading] = useState(state.loading);
   return (
@@ -12,19 +22,15 @@ const SignLoading = ({state, type}) => {
       isVisible={state.loading}
       onBackdropPress={() => setLoading(!loading)}>
       {state.done ? (
-        <Icon style={styles.loadingIcon} size={68} name="checkmark-circle-outline" />
+        <Icon
+          style={styles.loadingIcon}
+          size={68}
+          name="checkmark-circle-outline"
+        />
       ) : (
         <ActivityIndicator size={64} color="#5566dd" />
       )}
-      <Text style={styles.loadingText}>
-        {type === 'signup'
-          ? state.done
-            ? 'Signed up'
-            : 'Signin up...'
-          : state.done
-          ? 'Signed in'
-          : 'Signing in'}
-      </Text>
+      <Text style={styles.loadingText}>{getOverlayText(type, state.done)}</Text>
     </Overlay>
   );
 };
